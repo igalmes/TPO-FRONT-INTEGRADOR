83a42655bab384317ef3048479eda0cd3ea42d94
Manuel Ignacio Galmes Armendariz <ignaciogalmes79@gmail.com>
2021-10-27 11:00:50 -0300

    Galeria de Imagenes

---
+++ archivos.js/index.js
@@ -0,0 +1,8 @@
+const formulario = document.getElementsByClassName('formularioContacto');
+const inputs = document.querySelectorAll('#formularioContacto input');
+
+inputs.forEach((input.addEvenListener))
+formulario.addEvenListener('Submit',(e) => {
+    e.prevent
+
+})
